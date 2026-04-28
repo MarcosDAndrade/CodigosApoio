20aec1e9fae90615d1a67dc19655b1556b9174a1
Filho <marcos.filho@capgemini.com>
2026-04-28 11:01:50 -0300

    Many-to-Many association with JoinTable

--- course/src/main/java/com/educandoweb/course/config/TestConfig.java
+++ course/src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -1,10 +1,14 @@
 package com.educandoweb.course.config;
 
 
+import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.entities.Product;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.entities.enums.OrderStatus;
+import com.educandoweb.course.repositories.CategoryRepository;
 import com.educandoweb.course.repositories.OrderRepository;
+import com.educandoweb.course.repositories.ProductRepository;
 import com.educandoweb.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -21,14 +25,41 @@ import java.util.Arrays;
 public class TestConfig implements CommandLineRunner {
 
     @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
     private UserRepository userRepository;
 
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
 
     @Override
     public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Eletronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+
+        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet,", 120.0, "ipsum");
+        Product p2 = new Product(null, "SmartTv", "Lorem ipsum dolor sit amet,", 1200.00, "ipsum");
+        Product p3 = new Product(null, "MacBookPro", "Lorem ipsum dolor sit amet,", 2000.0, "ipsum");
+        Product p4 = new Product(null, "Pc Gamer", "Lorem ipsum dolor sit amet,", 5000.0, "ipsum");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+        productRepository.saveAll(Arrays.asList(p1,p2,p3,p4));
+
+        p1.getCategories().add(cat2);
+        p2.getCategories().add(cat1);
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+
+        productRepository.saveAll(Arrays.asList(p1,p2,p3,p4));
+
         User u1 = new User(null, "Maria Brown", "Maria@gmail.com", "9999999", "123456");
         User u2 = new User(null, "Alex Green", "Alex@gmail.com", "99877777", "123456");
 
