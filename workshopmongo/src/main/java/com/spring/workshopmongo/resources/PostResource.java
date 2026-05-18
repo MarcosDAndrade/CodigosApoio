4465d144b368c1455f533e885e32e59a967f6534
marcfilh <marcos.filho@capgemini.com>
2026-05-18 13:44:24 -0300

    Consulta simples com query mapping

--- workshopmongo/src/main/java/com/spring/workshopmongo/resources/PostResource.java
+++ workshopmongo/src/main/java/com/spring/workshopmongo/resources/PostResource.java
@@ -2,10 +2,13 @@ package com.spring.workshopmongo.resources;
 
 import com.spring.workshopmongo.domain.Post;
 import com.spring.workshopmongo.domain.User;
+import com.spring.workshopmongo.resources.util.URL;
 import com.spring.workshopmongo.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -20,4 +23,13 @@ public class PostResource {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
+
+
 }
