8ed9aa71d7a51db84b50620471ed4bdd63f761c9
marcfilh <marcos.filho@capgemini.com>
2026-06-01 10:06:52 -0300

    Deletando branches

--- TestePR/src/main/java/Main.java
+++ TestePR/src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Esse é o segundo PR");
+        System.out.println("Apaguei as branches");
     }
 }
